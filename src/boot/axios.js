88c1d57e22090f0e6be29e1bca704ebfb7941540
Juan Sierra <juan@totally.group>
2023-03-24 13:01:20 +0100

    Remove unnecessary console.log

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -12,8 +12,6 @@ const store = useAuthStore();
 // for each client)
 const api = axios.create({ baseURL: process.env.API_URL });
 
-console.log('API_URL:', process.env.API_URL);
-
 export default boot(() => {
   /** { app } */
   api.interceptors.request.use(async (req) => {
