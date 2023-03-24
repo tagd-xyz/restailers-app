72ecfa5cce22a714bc2ace04e04e587ac086f275
Juan Sierra <juan@totally.group>
2023-03-24 23:06:20 +0100

    Add .env vars for Firebase config

--- build/prepare.sh
+++ build/prepare.sh
@@ -14,14 +14,43 @@ fi
 case ${ENV} in
   production)
     api_hostname="api.tagd.com"
+    firebaseApiKey=""
+    firebaseAuthDomain=""
+    firebaseProjectId=""
+    firebaseStorageBucket=""
+    firebaseMessagingSenderId=""
+    firebaseAppId=""
+    firebaseMeasurementId=""
     ;;
 
   qa|uat)
     api_hostname="tagd-ret-api.${ENV}.tclstaging.com"
+    firebaseApiKey="FIREBASE_API_KEY_${ENV}"
+    firebaseApiKey=${!firebaseApiKey}
+    firebaseAuthDomain="FIREBASE_AUTH_DOMAIN_${ENV}"
+    firebaseAuthDomain=${!firebaseAuthDomain}
+    firebaseProjectId="FIREBASE_PROJECT_ID_${ENV}"
+    firebaseProjectId=${!firebaseProjectId}
+    firebaseStorageBucket="FIREBASE_STORAGE_BUCKET_${ENV}"
+    firebaseStorageBucket=${!firebaseStorageBucket}
+    firebaseMessagingSenderId="FIREBASE_MESSAGING_SENDER_ID_${ENV}"
+    firebaseMessagingSenderId=${!firebaseMessagingSenderId}
+    firebaseAppId="FIREBASE_APP_ID_${ENV}"
+    firebaseAppId=${!firebaseAppId}
+    firebaseMeasurementId="FIREBASE_MEASUREMENT_ID_${ENV}"
+    firebaseMeasurementId=${!firebaseMeasurementId}
     ;;
 esac
 
 API_URL=https://${api_hostname}/api/v1/
+
+FIREBASE_API_KEY=${firebaseApiKey},
+FIREBASE_AUTH_DOMAIN=${firebaseAuthDomain},
+FIREBASE_PROJECT_ID=${firebaseProjectId},
+FIREBASE_STORAGE_BUCKET=${firebaseStorageBucket},
+FIREBASE_MESSAGING_SENDER_ID=${firebaseMessagingSenderId},
+FIREBASE_APP_ID=${firebaseAppId},
+FIREBASE_MEASUREMENT_ID=${firebaseMeasurementId}
 
 echo "ENV: ${ENV}"
 echo "API_URL: ${API_URL}"
